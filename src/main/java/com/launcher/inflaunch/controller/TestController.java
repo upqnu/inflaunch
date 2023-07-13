39e536520080f5a6a30b908f130f07f9952bafcf
upqnu <upgrdrqnu@gmail.com>
2023-07-14 01:23:59 +0900

    Thymeleaf setting

---
+++ src/main/java/com/launcher/inflaunch/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.launcher.inflaunch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value = "/hello")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello(
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model
+    ) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
