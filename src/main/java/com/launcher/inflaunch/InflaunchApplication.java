ae0b90e9135d59641f496a9d8ed0562a15293834
upqnu <upgrdrqnu@gmail.com>
2023-07-13 17:42:49 +0900

    init commit

---
+++ src/main/java/com/launcher/inflaunch/InflaunchApplication.java
@@ -0,0 +1,13 @@
+package com.launcher.inflaunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflaunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflaunchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/launcher/inflaunch/InflaunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launcher.inflaunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflaunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
