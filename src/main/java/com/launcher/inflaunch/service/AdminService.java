478c84354f1effce53963de78a9e5b4073586f24
upqnu <upgrdrqnu@gmail.com>
2023-09-25 15:50:46 +0900

    feat : 수강평신고 조회 - 수강평 상태 변경 버튼 fix

--- src/main/java/com/launcher/inflaunch/service/AdminService.java
+++ src/main/java/com/launcher/inflaunch/service/AdminService.java
@@ -38,11 +38,8 @@ public class AdminService {
         Page<ReportReview> reportedReviewsPage = reportReviewRepository.findAll(pageable);
         List<ReportReviewInfoDto> reportReviewInfoDtos = new ArrayList<>();
 
-        log.info("plz : " + reportedReviewsPage);
-
         if (reportedReviewsPage.hasContent()) {
             for (ReportReview reportReview : reportedReviewsPage.getContent()) {
-                log.info("허허허 : " + reportedReviewsPage.getContent());
                 Review reportedReview = reportReview.getReview();
 
                 /* 신고되지 않은 수강평에 대한 정보는 노출할 필요가 없다. 하나라도 신고가 접수되면 admin이 확인할 수 있도록 한다. */
@@ -53,8 +50,6 @@ public class AdminService {
                     String reportReviewContent = reportReview.getContent();
                     Long reviewReportingUserId = reportReview.getUser().getId();
                     String reviewReportingUsername = reportReview.getUser().getUsername();
-
-                    log.info("for문 내부에서 수강평신고 정보 : " + reviewReportingUserId);
 
                     // 신고된 review 정보
                     Long reportedReviewId = reportedReview.getId();
@@ -72,14 +67,11 @@ public class AdminService {
                             reportedReviewId, reportedReviewWrite, reviewUserId, reviewUsername,
                             reportedReviewCourseId, reportedReviewCourseTitle, reportCount, reviewStatus
                     );
-                    log.info("Created ReportReviewInfoDto: {}", reportReviewInfoDto);
                     reportReviewInfoDtos.add(reportReviewInfoDto);
-                    log.info("plz 2 : {}", reportReviewInfoDtos);
                 }
             }
         }
 
-//        return reportReviewInfoDtos;
         return reportedReviewsPage.getTotalElements() == 0? new PageImpl<>(Collections.emptyList(), pageable, reportedReviewsPage.getTotalElements()) : new PageImpl<>(reportReviewInfoDtos, pageable, reportedReviewsPage.getTotalElements());
     }
 
@@ -89,8 +81,12 @@ public class AdminService {
         Review readyToBeUpdatedReview = reviewRepository.findById(reviewId)
                 .orElseThrow(() -> new ReviewNotFoundException(reviewId + "번 ID의 수강평이 존재하지 않습니다."));
 
+        log.info("2. Status 수정 시작");
+
         readyToBeUpdatedReview.setRegDate(readyToBeUpdatedReview.getRegDate());
         readyToBeUpdatedReview.setReviewStatus(reviewStatus);
+
         reviewRepository.save(readyToBeUpdatedReview);
+        log.info("3. ID {}번 수강평은 {} 처리하였습니다.", reviewId, reviewStatus);
     }
 }
